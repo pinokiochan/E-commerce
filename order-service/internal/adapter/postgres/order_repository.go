a1e7f23f5500eeb6c119a19f8032eee1f42879b1
pinokiochan <beibarys7ergaliev@gmail.com>
2025-04-08 06:06:09 +0500

    dao create is implemented

--- order-service/internal/adapter/postgres/order_repository.go
+++ order-service/internal/adapter/postgres/order_repository.go
@@ -16,5 +16,34 @@ func NewOrderRepository(db *pgxpool.Pool) *Order {
 }
 
 func (r *Order) Create(ctx context.Context, order models.Order) (int64, error) {
+	tx, err := r.db.Begin(ctx)
+	if err != nil {
+		return 0, err
+	}
+	defer tx.Rollback(ctx)
+	query := `
+			INSERT INTO orders (customername, status)
+			VALUES ($1, $2)
+			RETURNING ID;
+	`
+	var orderID int64
+	err = tx.QueryRow(ctx, query, order.CustomerName, order.Status).Scan(&orderID)
+	if err != nil {
+		return 0, err
+	}
+	queryOrderItems := `
+				INSERT INTO order_items (OrderID, ProductID, Quantity) VALUES
+				($1, $2, $3)
+				ON CONFLICT (OrderID, ProductID)
+				DO UPDATE SET Quantity = order_items.Quantity + EXCLUDED.Quantity
+	`
+	for _, v := range order.OrderItems {
+		_, err = tx.Exec(ctx, queryOrderItems, orderID, v.ProductID, v.Quantity)
+		if err != nil {
+			return 0, err
+		}
+
+	}
+	return orderID, tx.Commit(ctx)
 
 }
