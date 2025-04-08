ba88104caa06013fd1351813529f36bd8bfc3efb
pinokiochan <beibarys7ergaliev@gmail.com>
2025-04-08 19:23:24 +0500

    models defined

---
+++ inventory-service/internal/models/filters.go
@@ -0,0 +1,48 @@
+package models
+
+import (
+	"api-gateway/pkg/validator"
+	"slices"
+	"strings"
+)
+
+type Filters struct {
+	Page         int
+	PageSize     int
+	Sort         string
+	SortSafelist []string
+}
+
+func ValidateFilters(e *validator.Validator, f Filters) {
+	e.Check(f.Page > 0, "page", "must be greater than zero")
+	e.Check(f.PageSize <= 10_000_000, "page", "must be maximum of 10 million")
+	e.Check(f.PageSize > 0, "page", "must be greater than 0")
+	e.Check(f.PageSize <= 100, "page_size", "must be maximum of 100")
+
+	e.Check(validator.PermittedValue(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+
+}
+
+
+func (f Filters) SortColumn() string {
+	if slices.Contains(f.SortSafelist, f.Sort) {
+		return strings.TrimPrefix(f.Sort, "-")
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+// Return the sort direction ("ASC" or "DESC") depending on the prefix character of the
+// Sort field.
+func (f Filters) SortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
+}
+
+func (f Filters) Limit() int {
+	return f.PageSize
+}
+
+func (f Filters) Offset() int {
+	return (f.Page - 1) * f.PageSize
+}
---
+++ inventory-service/internal/models/inventory.go
@@ -0,0 +1,25 @@
+package models
+
+import "time"
+
+type Inventory struct {
+	ID          int64
+	Name        string
+	Description string
+	Price       float64
+	Available   int64
+	CreatedAt   time.Time
+	Version     int
+	IsDeleted   bool
+}
+
+type UpdateInventoryData struct {
+	ID          *int64
+	Name        *string
+	Description *string
+	Price       *float64
+	Available   *int64
+	CreatedAt   *time.Time
+	Version     *int
+	IsDeleted   *bool
+}
