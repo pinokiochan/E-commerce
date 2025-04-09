314873eefdc51e1ad35eda3d8f16d93576197d9f
pinokiochan <beibarys7ergaliev@gmail.com>
2025-04-09 21:54:40 +0500

    inventory service is done

--- inventory-service/cmd/inventory/main.go
+++ inventory-service/cmd/inventory/main.go
@@ -1,34 +1,34 @@
 package main
 
-// import (
-// 	"context"
-// 	"inventory-service/config"
-// 	"inventory-service/internal/app"
-// 	"log"
-// )
+import (
+	"context"
+	"inventory-service/config"
+	"inventory-service/internal/app"
+	"log"
+)
 
-// func main() {
-// 	ctx := context.Background()
+func main() {
+	ctx := context.Background()
 
-// 	// Parse config
-// 	cfg, err := config.New()
-// 	if err != nil {
-// 		log.Printf("failed to parse config: %v", err)
+	// Parse config
+	cfg, err := config.New()
+	if err != nil {
+		log.Printf("failed to parse config: %v", err)
 
-// 		return
-// 	}
+		return
+	}
 
-// 	application, err := app.New(ctx, cfg)
-// 	if err != nil {
-// 		log.Println("failed to setup application:", err)
+	application, err := app.New(ctx, cfg)
+	if err != nil {
+		log.Println("failed to setup application:", err)
 
-// 		return
-// 	}
+		return
+	}
 
-// 	err = application.Run()
-// 	if err != nil {
-// 		log.Println("failed to run application: ", err)
+	err = application.Run()
+	if err != nil {
+		log.Println("failed to run application: ", err)
 
-// 		return
-// 	}
-// }
+		return
+	}
+}
--- inventory-service/internal/app/app.go
+++ inventory-service/internal/app/app.go
@@ -1,1 +1,84 @@
 package app
+
+import (
+	"context"
+	"fmt"
+	"inventory-service/config"
+	httpservice "inventory-service/internal/adapter/http/service"
+	postgresrepo "inventory-service/internal/adapter/postgres"
+	"inventory-service/internal/usecase"
+	"inventory-service/pkg/postgres"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+const serviceName = "Inventory"
+
+type Application struct {
+	httpServer *httpservice.API
+	postgresDB *postgres.PostgreDB
+	// grpcServer *grpc.Server // Example
+}
+
+func New(ctx context.Context, config *config.Config) (*Application, error) {
+	log.Printf("starting %v service\n", serviceName)
+	log.Println("connecting to postgres")
+
+	postgresDB, err := postgres.New(ctx, config.Postgres)
+	if err != nil {
+		return nil, fmt.Errorf("mongo: %w", err)
+	}
+	log.Println("connection established")
+
+	inventoryRepo := postgresrepo.NewInventoryRepository(postgresDB.Pool)
+
+	inventoryUseCase := usecase.NewInventory(inventoryRepo)
+	httpServer := httpservice.New(config.Server, inventoryUseCase)
+
+	app := &Application{
+		httpServer: httpServer,
+		postgresDB: postgresDB,
+	}
+
+	return app, nil
+}
+
+func (a *Application) Close() {
+	// Closing http server
+	err := a.httpServer.Stop()
+
+	// Closing postgres connection
+	a.postgresDB.Pool.Close()
+
+	if err != nil {
+		log.Println("failed to shutdown service", err)
+	}
+}
+
+func (app *Application) Run() error {
+	errCh := make(chan error, 1)
+
+	// Running http server
+	app.httpServer.Run(errCh)
+
+	log.Printf("service %v started\n", serviceName)
+
+	// Waiting signal
+	shutdownCh := make(chan os.Signal, 1)
+	signal.Notify(shutdownCh, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case errRun := <-errCh:
+		return errRun
+
+	case s := <-shutdownCh:
+		log.Printf("received signal: %v. Running graceful shutdown...\n", s)
+
+		app.Close()
+		log.Println("graceful shutdown completed!")
+	}
+
+	return nil
+}
