4893b209a9323cf6daf91a4c3ee5b514f8a3a1a1
pinokiochan <beibarys7ergaliev@gmail.com>
2025-04-09 18:59:20 +0500

    table is defined

--- inventory-service/migrations/000001_create_inventory.up.sql
+++ inventory-service/migrations/000001_create_inventory.up.sql
@@ -1,10 +1,10 @@
-``CREATE TABLE inventory (
+CREATE TABLE inventory (
     id bigserial PRIMARY KEY,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
     name VARCHAR(50) NOT NULL,
     description TEXT NOT NULL,
     price NUMERIC(10, 2) NOT NULL CHECK(price > 0),
-    available integer DEFAULT 0 CHECK(available > 0)
+    available integer DEFAULT 0 CHECK(available >= 0),
     isdeleted BOOLEAN DEFAULT 'FALSE',
     version integer NOT NULL DEFAULT 1
-);``
+);
