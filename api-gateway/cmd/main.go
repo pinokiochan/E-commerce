d26e66fc8de09f321da5315284fe96878a9a6fd9
pinokiochan <beibarys7ergaliev@gmail.com>
2025-04-09 21:55:21 +0500

    api gateway is done

--- api-gateway/cmd/main.go
+++ api-gateway/cmd/main.go
@@ -1,5 +1,73 @@
 package main
 
+import (
+	"api-gateway/config"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	
+	config := config.New()
+
+	r := gin.Default()
+
+	r.Any("/*proxyPath", func(c *gin.Context) {
+		targetURL := ""
+		path := c.Param("proxyPath")
+		rawQuery := c.Request.URL.RawQuery
+
+		// Выбор сервиса в зависимости от пути
+		if strings.HasPrefix(path, "/orders") {
+			targetURL = config.OrderService.Addr + path
+		} else if strings.HasPrefix(path, "/products") {
+			targetURL = config.InventoryService.Addr + path
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{"error": "unknown route"})
+			return
+		}
+
+		// Добавляем query параметры, если они есть
+		if rawQuery != "" {
+			targetURL += "?" + rawQuery
+		}
+
+		// Создаём новый запрос
+		req, err := http.NewRequest(c.Request.Method, targetURL, c.Request.Body)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "cannot create request"})
+			return
+		}
+
+		// Копируем заголовки
+		for key, values := range c.Request.Header {
+			for _, value := range values {
+				req.Header.Add(key, value)
+			}
+		}
+
+		// Отправляем запрос
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			c.JSON(http.StatusBadGateway, gin.H{"error": "target service unavailable"})
+			return
+		}
+		defer resp.Body.Close()
+
+		// Копируем заголовки ответа
+		for k, vv := range resp.Header {
+			for _, v := range vv {
+				c.Writer.Header().Add(k, v)
+			}
+		}
+
+		// Устанавливаем статус и возвращаем тело ответа
+		c.Status(resp.StatusCode)
+		io.Copy(c.Writer, resp.Body)
+	})
+
+	r.Run(":8080") // API Gateway будет слушать здесь
 }
--- api-gateway/config/config.go
+++ api-gateway/config/config.go
@@ -1,2 +1,39 @@
-package cmd
+package config
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type (
+	Config struct {
+		OrderService     OrderService
+		InventoryService InventoryService
+	}
+
+	OrderService struct {
+		Addr string
+	}
+
+	InventoryService struct {
+		Addr string
+	}
+)
+
+func New() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error: %v", err.Error())
+	}
+
+	return &Config{
+		OrderService: OrderService{
+			Addr: os.Getenv("ORDER_SERVICE"),
+		},
+		InventoryService: InventoryService{
+			Addr: os.Getenv("INVENTORY_SERVICE"),
+		},
+	}
+}
