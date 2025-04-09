7984c49017fe4c5bd50f790276d1279cfb03b80a
pinokiochan <beibarys7ergaliev@gmail.com>
2025-04-09 21:48:09 +0500

    usecase is defined and implemented

--- order-service/internal/usecase/interface.go
+++ order-service/internal/usecase/interface.go
@@ -6,9 +6,13 @@ import (
 )
 
 type OrderRepository interface {
-	CreateOrder(ctx context.Context, order models.Order) (int64, error)
-	GetByFilter(ctx context.Context, filter models.OrderFilter) (models.Order, error)
-	GetListOrderByFilter(ctx context.Context, filter models.OrderFilter) ([]models.Order, error)
-	UpdateOrder(ctx context.Context, update models.OrderUpdateData) error
+	Create(ctx context.Context, order models.Order) (int64, error)
+	GetWithFilter(ctx context.Context, filter models.OrderFilter) (models.Order, error)
+	GetListWithFilter(ctx context.Context, filter models.OrderFilter) ([]models.Order, error)
+	Update(ctx context.Context, update models.OrderUpdateData) error
+}
 
+type InventoryService interface {
+	GetById(id int64) (models.Inventory, error)
+	Substruct(id, newAvailability int64, version int32) error
 }
--- order-service/internal/usecase/order.go
+++ order-service/internal/usecase/order.go
@@ -2,59 +2,118 @@ package usecase
 
 import (
 	"context"
+	"fmt"
 	"order-service/internal/models"
 )
 
 type Order struct {
-	orderRepo OrderRepository
+	orderRepo        OrderRepository
+	inventoryService InventoryService
 }
 
-func NewOrder(orderRepo OrderRepository) *Order {
+func NewOrder(orderRepo OrderRepository, inventoryService InventoryService) *Order {
 	return &Order{
-		orderRepo: orderRepo,
+		orderRepo:        orderRepo,
+		inventoryService: inventoryService,
 	}
 }
 
-func (u *Order) Create(ctx context.Context, request models.Order) (models.Order, error) {
-	// fmt.Println(request.OrderItems[0].Quantity)
-	orderID, err := u.orderRepo.CreateOrder(ctx, request)
+func (u *Order) Create(ctx context.Context, request models.Order) (models.OrderResponce, error) {
+	// Inserting order to database
+	orderID, err := u.orderRepo.Create(ctx, request)
 	if err != nil {
-		return models.Order{}, err
+		return models.OrderResponce{}, err
 	}
-	request.ID = orderID
-	return request, nil
-}
 
-func (u *Order) Get(ctx context.Context, id int64) (models.Order, error) {
+	// Metadata of items
+	var orderItemResponces []models.OrderItemResponce
+	var totalPrice int64
+	for _, item := range request.OrderItems {
+		var orderItemResp models.OrderItemResponce
+		orderItemResp.ProductID = item.ProductID
 
-	order, err := u.orderRepo.GetByFilter(ctx, models.OrderFilter{ID: id})
-	if err != nil {
-		return models.Order{}, err
+		// Getting inventory from inventory service
+		inventoryItem, err := u.inventoryService.GetById(item.ProductID)
+		if err != nil {
+			orderItemResp.Status = "rejected"
+			orderItemResp.Reason = err.Error()
+			orderItemResponces = append(orderItemResponces, orderItemResp)
+			continue
+		}
+
+		price := inventoryItem.Price * item.Quantity
+		newAvailability := inventoryItem.Available - item.Quantity
+
+		if newAvailability < 0 {
+			orderItemResp.Status = "rejected"
+			orderItemResp.Reason = "insufficient_inventory"
+			orderItemResponces = append(orderItemResponces, orderItemResp)
+			continue
+		}
+
+		// Trying to set new availability
+		err = u.inventoryService.Substruct(item.ProductID, newAvailability, inventoryItem.Version)
+		if err != nil {
+			orderItemResp.Status = "rejected"
+			orderItemResp.Reason = err.Error()
+			orderItemResponces = append(orderItemResponces, orderItemResp)
+			continue
+		}
+
+		orderItemResp.Name = inventoryItem.Name
+		orderItemResp.Price = price
+		orderItemResp.Status = "accepted"
+
+		totalPrice += price
+
+		orderItemResponces = append(orderItemResponces, orderItemResp)
 	}
-	return order, nil
+
+	fmt.Println(orderItemResponces)
+
+	responce := models.OrderResponce{
+		OrderID:      orderID,
+		CustomerName: request.CustomerName,
+		Items:        orderItemResponces,
+		Total:        totalPrice,
+	}
+
+	return responce, nil
 }
 
 func (u *Order) GetList(ctx context.Context) ([]models.Order, error) {
-	orders, err := u.orderRepo.GetListOrderByFilter(ctx, models.OrderFilter{})
+	orders, err := u.orderRepo.GetListWithFilter(ctx, models.OrderFilter{})
 	if err != nil {
 		return nil, err
 	}
 	return orders, nil
 }
 
-func (u *Order) SetStatus(ctx context.Context, request models.UpdateStatus) (models.Order, error) {
-	order, err := u.Get(ctx, request.OrderID)
-	if err != nil {
-		return models.Order{}, nil
-	}
-	var updatedOrder models.OrderUpdateData
-	updatedOrder.ID = &order.ID
-	updatedOrder.Status = &request.Status
-	err = u.orderRepo.UpdateOrder(ctx, updatedOrder)
+func (u *Order) Get(ctx context.Context, id int64) (models.Order, error) {
+	order, err := u.orderRepo.GetWithFilter(ctx, models.OrderFilter{ID: id})
 	if err != nil {
 		return models.Order{}, err
 	}
-	order.Status = request.Status
 
+	return order, nil
+}
+
+func (u *Order) SetStatus(ctx context.Context, req models.UpdateStatus) (models.Order, error) {
+	order, err := u.Get(ctx, req.OrderID)
+	if err != nil {
+		return models.Order{}, err
+	}
+
+	var updatedOrder models.OrderUpdateData
+	updatedOrder.ID = &order.ID
+	updatedOrder.Status = &req.Status
+
+	err = u.orderRepo.Update(ctx, updatedOrder)
+
+	if err != nil {
+		return models.Order{}, err
+	}
+
+	order.Status = req.Status
 	return order, nil
 }
