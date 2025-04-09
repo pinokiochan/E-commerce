9d1f9531b6e1ed03b86d2b0853be1d84c2b78f8e
pinokiochan <beibarys7ergaliev@gmail.com>
2025-04-09 18:59:04 +0500

    rep logic is defined

--- inventory-service/internal/usecase/interface.go
+++ inventory-service/internal/usecase/interface.go
@@ -1,14 +1,14 @@
 package usecase
 
 import (
-	"inventory-service/internal/models"
 	"context"
+	"inventory-service/internal/models"
 )
 
 type InventoryRepository interface {
 	CreateItem(ctx context.Context, item models.Inventory) (int64, error)
 	Get(ctx context.Context, id int64) (models.Inventory, error)
-	GetListInventory(ctx context.Context, filters models.Filters) ([]models.Inventory, error)
-	Update(ctx context.Context, item models.Inventory) error
+	GetListInventory(ctx context.Context, filters models.Filters) ([]models.Inventory, int, error)
+	Update(ctx context.Context, item *models.Inventory) error
 	Delete(ctx context.Context, id int64) error
 }
