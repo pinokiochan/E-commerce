4fd5ff888544fd7ed47450af7146c7a023e508b4
pinokiochan <beibarys7ergaliev@gmail.com>
2025-04-09 21:50:25 +0500

    pkg is setted

--- order-service/pkg/postgres/postgres.go
+++ order-service/pkg/postgres/postgres.go
@@ -11,6 +11,7 @@ type PostgreDB struct {
 	Pool     *pgxpool.Pool
 	DBConfig *pgxpool.Config
 }
+
 type Config struct {
 	Dsn          string `env:"POSTGRES_DSN,required"`
 	MaxOpenConns int32  `env:"POSTGRES_MAX_OPEN_CONN" envDefault:"25"`
@@ -23,21 +24,27 @@ func New(ctx context.Context, config Config) (*PostgreDB, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	dbConfig.MaxConns = config.MaxOpenConns
+
 	// Use the time.ParseDuration() function to convert the idle timeout duration string
 	// to a time.Duration type.
 	duration, err := time.ParseDuration(config.MaxIdleTime)
 	if err != nil {
 		return nil, err
 	}
+
 	dbConfig.MaxConnIdleTime = duration
+
 	pool, err := pgxpool.NewWithConfig(ctx, dbConfig)
 	if err != nil {
 		return nil, err
 	}
+
 	if err = pool.Ping(ctx); err != nil {
 		return nil, err
 	}
+
 	return &PostgreDB{
 		Pool:     pool,
 		DBConfig: dbConfig,
