b41f52ae0095d7b1b9d6645f6b51ee46ccce45f4
pinokiochan <beibarys7ergaliev@gmail.com>
2025-04-09 21:54:16 +0500

    handler dto is implemented

--- inventory-service/internal/adapter/http/service/handlers/dto/inventory.go
+++ inventory-service/internal/adapter/http/service/handlers/dto/inventory.go
@@ -1,7 +1,11 @@
 package dto
 
 import (
+	"inventory-service/internal/adapter/postgres/dao"
 	"inventory-service/internal/models"
+	"inventory-service/pkg/validator"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,6 +20,23 @@ type InventoryCreateRequest struct {
 type InventoryCreateResponse struct {
 	ID   int64  `json:"id"`
 	Name string `json:"name"`
+}
+
+type InventoryUpdateRequest struct {
+	Name        *string  `json:"name"`
+	Description *string  `json:"description"`
+	Price       *float64 `json:"price"`
+	Available   *int64   `json:"available"`
+}
+
+type InventoryResponse struct {
+	ID          int64     `json:"id,omitempty"`
+	Name        string    `json:"name,omitempty"`
+	Description string    `json:"description,omitempty"`
+	Price       float64   `json:"price,omitempty"`
+	Available   int64     `json:"available,omitempty"`
+	CreatedAt   time.Time `json:"created_at,omitzero"`
+	Version     int32     `json:"version,omitempty"`
 }
 
 func ToInventoryCreateRequest(ctx *gin.Context) (models.Inventory, error) {
@@ -36,9 +57,91 @@ func ToInventoryCreateRequest(ctx *gin.Context) (models.Inventory, error) {
 	return inventory, nil
 }
 
+func ToInventoryUpdateRequest(ctx *gin.Context) (models.UpdateInventoryData, error) {
+	id, err := ReadParamID(ctx)
+	if err != nil {
+		return models.UpdateInventoryData{}, err
+	}
+
+	var req InventoryUpdateRequest
+	err = ctx.ShouldBindJSON(&req)
+	if err != nil {
+		return models.UpdateInventoryData{}, err
+	}
+
+	var inventory models.UpdateInventoryData
+
+	expectedVersion := ctx.GetHeader("X-Expected-Version")
+
+	if expectedVersion != "" {
+		expectedVersionInt, err := strconv.ParseInt(expectedVersion, 10, 32)
+		if err != nil {
+			return models.UpdateInventoryData{}, err
+		}
+		result := int32(expectedVersionInt)
+		inventory.Version = &result
+	}
+
+	inventory.ID = &id
+	inventory.Name = req.Name
+	inventory.Description = req.Description
+	inventory.Price = req.Price
+	inventory.Available = req.Available
+
+	return inventory, nil
+}
+
 func ToInventoryCreateResponse(inv models.Inventory) InventoryCreateResponse {
 	return InventoryCreateResponse{
 		ID:   inv.ID,
 		Name: inv.Name,
 	}
+}
+
+func ToInventoryListResponse(invs []models.Inventory) []InventoryResponse {
+	var responce []InventoryResponse
+
+	for _, v := range invs {
+		responce = append(responce, ToInventoryResponse(v))
+	}
+
+	return responce
+}
+
+func ToInventoryResponse(inv models.Inventory) InventoryResponse {
+	return InventoryResponse{
+		ID:          inv.ID,
+		Name:        inv.Name,
+		Description: inv.Description,
+		Price:       inv.Price,
+		Available:   inv.Available,
+		CreatedAt:   inv.CreatedAt,
+		Version:     inv.Version,
+	}
+}
+
+func ParseListRequest(ctx *gin.Context, v *validator.Validator) models.Filters {
+	filter := models.Filters{
+		Page:         1,                // Default current page
+		PageSize:     8,                // Default page size
+		Sort:         "id",             // Default sort value
+		SortSafelist: dao.SafeSortList, // Available sort options
+	}
+
+	// Parse page parameter
+	filter.Page = ReadInt(ctx, "page", filter.Page, v)
+
+	// Parse page_size parameter
+	filter.PageSize = ReadInt(ctx, "page_size", filter.PageSize, v)
+
+	// Parse sort parameter
+	filter.Sort = ReadString(ctx, "sort", filter.Sort)
+
+	if sortParam := ctx.Query("sort"); sortParam != "" {
+		filter.Sort = sortParam
+	}
+
+	models.ValidateFilters(v, filter)
+
+	return filter
 }
--- inventory-service/internal/adapter/http/service/handlers/inventory_handler.go
+++ inventory-service/internal/adapter/http/service/handlers/inventory_handler.go
@@ -1,6 +1,12 @@
 package handlers
 
 import (
+	"errors"
+	"inventory-service/internal/adapter/http/service/handlers/dto"
+	"inventory-service/pkg/validator"
+	"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,21 +19,107 @@ func NewInventory(invUseCase InventoryUsecase) *Inventory {
 }
 
 func (h *Inventory) Create(ctx *gin.Context) {
+	inventory, err := dto.ToInventoryCreateRequest(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
+	v := validator.New()
+	if dto.ValidateInventory(v, inventory); !v.Valid() {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"error": v.Errors})
+		return
+	}
+
+	inventoryNew, err := h.invUseCase.CreateItem(ctx.Request.Context(), inventory)
+	if err != nil {
+		errCtx := dto.FromError(err)
+		ctx.JSON(errCtx.Code, gin.H{"error": errCtx.Message})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{"inventory": dto.ToInventoryCreateResponse(inventoryNew)})
 }
 
-func (h *Inventory) Get(ctx *gin.Context) {
+func (h *Inventory) GetList(ctx *gin.Context) {
+	v := validator.New()
 
+	filters := dto.ParseListRequest(ctx, v)
+	if !v.Valid() {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"error": v.Errors})
+		return
+	}
+
+	items, metadata, err := h.invUseCase.GetListInventory(ctx.Request.Context(), filters)
+	if err != nil {
+		errCtx := dto.FromError(err)
+		log.Println(err)
+		ctx.JSON(errCtx.Code, gin.H{"error": errCtx.Message})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"inventory": dto.ToInventoryListResponse(items),
+		"metadata":  metadata,
+	})
 }
 
-func (h *Inventory) GetListInventory(ctx *gin.Context) {
+func (h *Inventory) GetByID(ctx *gin.Context) {
+	id, err := dto.ReadParamID(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
+		return
+	}
 
+	inventory, err := h.invUseCase.Get(ctx.Request.Context(), id)
+	if err != nil {
+		log.Println(err)
+		errCtx := dto.FromError(err)
+		ctx.JSON(errCtx.Code, gin.H{"error": errCtx.Message})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"inventory": dto.ToInventoryResponse(inventory)})
 }
 
 func (h *Inventory) Update(ctx *gin.Context) {
+	item, err := dto.ToInventoryUpdateRequest(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
+	itemUpdated, err := h.invUseCase.Update(ctx.Request.Context(), item)
+	if err != nil {
+		if errors.Is(err, dto.ErrUnprocessableEntity) {
+			v := validator.New()
+			dto.ValidateInventory(v, itemUpdated)
+			if !v.Valid() {
+				ctx.JSON(http.StatusUnprocessableEntity, gin.H{"error": v.Errors})
+				return
+			}
+		}
+		errCtx := dto.FromError(err)
+		ctx.JSON(errCtx.Code, gin.H{"error": errCtx.Message})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"inventory": dto.ToInventoryResponse(itemUpdated)})
 }
 
 func (h *Inventory) Delete(ctx *gin.Context) {
+	id, err := dto.ReadParamID(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
+		return
+	}
 
+	err = h.invUseCase.Delete(ctx.Request.Context(), id)
+	if err != nil {
+		errCtx := dto.FromError(err)
+		ctx.JSON(errCtx.Code, gin.H{"error": errCtx.Message})
+		return
+	}
+
+	ctx.Status(http.StatusNoContent)
 }
